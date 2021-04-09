2628cc05ef0473dfcf68a978d06e5109fa840aca
Cooper.Wu <wutuo@chinamobile.com>
2021-04-09 14:29:57 +0800

    app和ui启动脚本

---
+++ scripts/app-shell-script.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+# sh app-shell-script.sh
+python3 ../app.py run --config-yaml config_files/.test_env_config_fact_store_es.yaml &
---
+++ scripts/ui-shell-script.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+# sh ui-shell-script.sh
+export CUSTOMER_ID="#1234567"
+export SQL_CONNECT="mysql+pymysql://zak:password@localhost/factstore"
+python3 ../app.py --metric-port 8081 ui --port 5001 &
