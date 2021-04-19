8c271b6d05cb4f043d01c902bf94680a08a10c0c
Cooper.Wu <wutuo@chinamobile.com>
2021-04-19 08:41:23 +0800

    删除暂时不适用的文件

--- scripts/run-lad-job.sh
+++
@@ -1,2 +0,0 @@
-time python ../app.py run --config-yaml ../config_files/.env_cmn_log_fmt_config.yaml --single-run True
-
--- scripts/run-web-ui.sh
+++
@@ -1,3 +0,0 @@
-export CUSTOMER_ID="#1234567"
-export SQL_CONNECT="mysql+pymysql://zak:password@localhost/factstore"
-python ../app.py --metric-port 8081 ui
