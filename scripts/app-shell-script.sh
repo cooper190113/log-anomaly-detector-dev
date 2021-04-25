17c47fd4f853dbf31f8bf11b76cc2c351bd5227b
Cooper.Wu <wutuo@chinamobile.com>
2021-04-25 11:18:52 +0800

    修改脚本启动路径

--- scripts/app-shell-script.sh
+++ scripts/app-shell-script.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 # sh app-shell-script.sh
-nohup python3 ../app.py run --config-yaml config_files/.test_env_config_fact_store_es.yaml > nohup.out 2>&1 &
+nohup python3 app.py run --config-yaml config_files/.test_env_config_fact_store_es.yaml > nohup.out 2>&1 &
--- scripts/ui-shell-script.sh
+++ scripts/ui-shell-script.sh
@@ -2,4 +2,4 @@
 # sh ui-shell-script.sh
 export CUSTOMER_ID="#1234567"
 export SQL_CONNECT="mysql+pymysql://zak:password@localhost/factstore"
-nohup python3 ../app.py --metric-port 8081 ui --port 5001 > nohup.out 2>&1 &
+nohup python3 app.py --metric-port 8081 ui --port 5001 > nohup.out 2>&1 &
